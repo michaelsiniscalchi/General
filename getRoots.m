61338b59f05cea1107e9fdc2a58c84de60832c47
Michael Siniscalchi <michael.siniscalchi@princeton.edu>
2021-06-02 20:31:24 -0400

    Update getRoots.m
    Added home desktop PC

--- getRoots.m
+++ getRoots.m
@@ -2,9 +2,9 @@ function dirs = getRoots()
 
 name = getenv('COMPUTERNAME');
 switch name
-    case 'homePC'
+    case 'STELLATE'
         dirs.root = fullfile('J:','Data & Analysis');
-       
+        dirs.code = fullfile('J:','Documents','GitHub');
     case 'PNI-F4W2YM2'
         dirs.root = fullfile('C:','Data');
         dirs.code = fullfile('C:','Users','mjs20','Documents','GitHub');
