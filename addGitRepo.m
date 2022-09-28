17c5cf1b54ce1dac362014b595c2343864c19fbd
Michael Siniscalchi <michael.siniscalchi@princeton.edu>
2022-09-28 16:02:26 -0400

    Update addGitRepo.m

--- addGitRepo.m
+++ addGitRepo.m
@@ -1,8 +1,12 @@
 %Must add the repo '...\General\' to MATLAB path before running (or save in Set Path settings)
 
 function dirs = addGitRepo(varargin)
+
 dirs = getRoots();
 for i = 1:numel(varargin)
     repo_directory = fullfile(dirs.code,varargin{i});
+    if exist(repo_directory,"dir")
     addpath(genpath(repo_directory));
+    disp(['Adding Search Path: ' repo_directory '...']);
+    end
 end
