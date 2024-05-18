7393ecc9d5d8eeb0549ec4643da97d5ee7f8682d
Michael Siniscalchi <michael.siniscalchi@princeton.edu>
2024-05-17 21:13:44 -0400

    Create MATLAB_path_script.m

---
+++ MATLAB_path_script.m
@@ -0,0 +1,6 @@
+%Set MATLAB paths
+addGitRepo(getRoots(),'General','TankMouseVR','U19-pipeline-matlab',...
+    'BrainCogs_mjs','iCorre-Registration','Image-Stack-Processing');
+
+%Default error-debugging behavior
+dbstop if error
