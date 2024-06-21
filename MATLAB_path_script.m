014bda8db1bde722a20223d607756b55108efe57
Michael Siniscalchi <michael.siniscalchi@princeton.edu>
2024-06-21 16:38:28 -0400

    pathing and plotting

--- MATLAB_path_script.m
+++ MATLAB_path_script.m
@@ -1,6 +1,6 @@
 %Set MATLAB paths
 addGitRepo(getRoots(),'General','TankMouseVR','U19-pipeline-matlab',...
-    'BrainCogs_mjs','iCorre-Registration','Image-Stack-Processing');
+    'BrainCogs_mjs','iCorre-Registration');
 
 %Default error-debugging behavior
 dbstop if error
